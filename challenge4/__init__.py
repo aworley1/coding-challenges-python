2ff81e266a76c3c2c999092dc3b105c363083641
Andrew Worley <andrew.worley@johnlewis.co.uk>
2019-05-02 22:43:21 +0100

    Extract sorting key to a named function

--- challenge4/__init__.py
+++ challenge4/__init__.py
@@ -1,6 +1,10 @@
 def sortVouchers(input):
     split_input = input.split(",")
 
-    sorted_input = sorted(split_input, key=lambda x: x.split(":")[2])
+    sorted_input = sorted(split_input, key=sorting_key)
 
     return ",".join(sorted_input)
+
+
+def sorting_key(voucher):
+    return voucher.split(":")[2]
