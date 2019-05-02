4fe8e72ec6e99b89f4d1ff3bb4c0acacf31efdec
Andrew Worley <andrew.worley@johnlewis.co.uk>
2019-05-02 22:32:06 +0100

    Sort the list just by the voucher id

--- challenge4/__init__.py
+++ challenge4/__init__.py
@@ -1,2 +1,6 @@
 def sortVouchers(input):
-    return input
+    split_input = input.split(",")
+
+    sorted_input = sorted(split_input, key=lambda x: x.split(":")[2])
+
+    return ",".join(sorted_input)
--- test_challenge4.py
+++ test_challenge4.py
@@ -15,15 +15,23 @@ class TestChallenge4(unittest.TestCase):
 
         assert len(result.split(",")) == 2
 
-    def test_return_list_if_already_sorted(self):
-        """
-        Should return original list if already sorted
-        """
-        input_list = "190111:Activated:ffff,190111:Available:cccc,190112:Activated:bbbb,190112:Available:aaaa,190110:Redeemded:dddd,190110:Expired:eeee"
+    # def test_return_list_if_already_sorted(self):
+    #     """
+    #     Should return original list if already sorted
+    #     """
+    #     input_list = "190111:Activated:ffff,190111:Available:cccc,190112:Activated:bbbb,190112:Available:aaaa,190110:Redeemded:dddd,190110:Expired:eeee"
+    #
+    #     result = sortVouchers(input_list)
+    #
+    #     assert input_list == result
 
-        result = sortVouchers(input_list)
+    def test_sort_by_voucher_id(self):
+        """
+        Should sort by voucher id
+        """
+        result = sortVouchers("190111:Activated:cccc,190101:Activated:bbbb,190102:Activated:aaaa")
 
-        assert input_list == result
+        assert result == "190102:Activated:aaaa,190101:Activated:bbbb,190111:Activated:cccc"
 
 
     if __name__ == '__main__':
