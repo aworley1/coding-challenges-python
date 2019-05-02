6b7ff68a814193670074fdba08aaf6f8c025f7f1
Andrew Worley <andrew.worley@johnlewis.co.uk>
2019-05-02 23:26:19 +0100

    Delete all code and start again now that I know more Python

--- challenge4/__init__.py
+++ challenge4/__init__.py
@@ -1,10 +1,2 @@
-def sortVouchers(input):
-    split_input = input.split(",")
-
-    sorted_input = sorted(split_input, key=sorting_key)
-
-    return ",".join(sorted_input)
-
-
-def sorting_key(voucher):
-    return voucher.split(":")[2]
+def sort_vouchers(input):
+    pass
--- test_challenge4.py
+++ test_challenge4.py
@@ -15,24 +15,5 @@ class TestChallenge4(unittest.TestCase):
 
         assert len(result.split(",")) == 2
 
-    # def test_return_list_if_already_sorted(self):
-    #     """
-    #     Should return original list if already sorted
-    #     """
-    #     input_list = "190111:Activated:ffff,190111:Available:cccc,190112:Activated:bbbb,190112:Available:aaaa,190110:Redeemded:dddd,190110:Expired:eeee"
-    #
-    #     result = sortVouchers(input_list)
-    #
-    #     assert input_list == result
-
-    def test_sort_by_voucher_id(self):
-        """
-        Should sort by voucher id
-        """
-        result = sortVouchers("190111:Activated:cccc,190101:Activated:bbbb,190102:Activated:aaaa")
-
-        assert result == "190102:Activated:aaaa,190101:Activated:bbbb,190111:Activated:cccc"
-
-
     if __name__ == '__main__':
         unittest.main()
